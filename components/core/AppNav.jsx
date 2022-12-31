bc5679cc00202aabea94f626dd0a339cbf4fb756
solandrust <zhanghe.1534@gmail.com>
2022-12-31 12:41:30 -0500

    refactor: disabled wallet connection, for now

--- components/core/AppNav.jsx
+++ components/core/AppNav.jsx
@@ -3,22 +3,22 @@ import NavLink from "./header/NavLink";
 export default function AppNav({ className = "" }) {
   return (
     <nav className={className}>
-      <NavLink href="/royalty">
-        {/* <img
+      {/* <NavLink href="/royalty">
+        <img
           src={"/icons/crown.svg"}
           alt={"Royalty Checker"}
           className="mr-3 icon-sm"
-        /> */}
+        />
         Royalty Checker
       </NavLink>
       <NavLink href="/fees">
-        {/* <img
+        <img
           src={"/icons/money-bag.svg"}
           alt={"Royalty Checker"}
           className="mr-3 icon-sm"
-        /> */}
+        />
         Fee Redeemer
-      </NavLink>
+      </NavLink> */}
       <NavLink href="/faucet">
         {/* <img
           src={"/icons/shower.svg"}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import React from "react";
 import App from "next/app";
 import { DefaultSeo } from "next-seo";
-import { ConnectionProvider } from "@solana/wallet-adapter-react";
+// import { ConnectionProvider } from "@solana/wallet-adapter-react";
 
 import "~/styles/globals.css";
 import dynamic from "next/dynamic";
@@ -9,49 +9,49 @@ import dynamic from "next/dynamic";
 // define the master endpoint for production
 const endpoint = "https://ssc-dao.genesysgo.net";
 
-const WalletProvider = dynamic(
-  () => import("~/contexts/ClientWalletProvider.jsx"),
-  {
-    ssr: false,
-  },
-);
+// const WalletProvider = dynamic(
+//   () => import("~/contexts/ClientWalletProvider.jsx"),
+//   {
+//     ssr: false,
+//   },
+// );
 
 export default class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
     return (
-      <ConnectionProvider endpoint={endpoint}>
-        <WalletProvider>
-          <React.Fragment>
-            <DefaultSeo
-              title="Solfate"
-              titleTemplate="Solfate - %s"
-              defaultTitle="Solfate"
-              openGraph={{
-                type: "website",
-                url: "https://solfate.com/",
-                site_name: "Solfate",
-                images: [
-                  {
-                    url: "https://solfate.com/icon.png",
-                    width: 256,
-                    height: 256,
-                    alt: "Solfate Labs",
-                  },
-                ],
-              }}
-              twitter={{
-                handle: "@SolfateLabs",
-                site: "@SolfateLabs",
-                cardType: "summary",
-                // cardType: "summary_large_image",
-              }}
-            />
+      // <ConnectionProvider endpoint={endpoint}>
+      //   <WalletProvider>
+      <React.Fragment>
+        <DefaultSeo
+          title="Solfate"
+          titleTemplate="Solfate - %s"
+          defaultTitle="Solfate"
+          openGraph={{
+            type: "website",
+            url: "https://solfate.com/",
+            site_name: "Solfate",
+            images: [
+              {
+                url: "https://solfate.com/icon.png",
+                width: 256,
+                height: 256,
+                alt: "Solfate Labs",
+              },
+            ],
+          }}
+          twitter={{
+            handle: "@SolfateLabs",
+            site: "@SolfateLabs",
+            cardType: "summary",
+            // cardType: "summary_large_image",
+          }}
+        />
 
-            <Component {...pageProps} />
-          </React.Fragment>
-        </WalletProvider>
-      </ConnectionProvider>
+        <Component {...pageProps} />
+      </React.Fragment>
+      //   </WalletProvider>
+      // </ConnectionProvider>
     );
   }
 }
