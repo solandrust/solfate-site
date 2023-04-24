c76676f55df31f3806410771805932c764bbbf50
solandrust <zhanghe.1534@gmail.com>
2023-04-23 20:44:33 -0400

    feat: ts

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
+/** @type {import('next').NextConfig} */
+
 const { withContentlayer } = require("next-contentlayer");
 
 module.exports = withContentlayer({
   reactStrictMode: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  compiler: {
+    styledComponents: true,
+  },
   swcMinify: true,
   // webpack5: true,
   webpack: (config) => {
@@ -9,4 +19,12 @@ module.exports = withContentlayer({
 
     return config;
   },
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: "https",
+  //       hostname: "**",
+  //     },
+  //   ],
+  // },
 });
